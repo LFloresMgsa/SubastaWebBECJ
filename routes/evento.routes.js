51f7479faa2529fb73b1aa443b961818d6de183f
miguel-ls <miguel.lopez@codesicorp.com>
2023-06-01 15:20:04 -0500

    .

--- routes/evento.routes.js
+++ routes/evento.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const webController  = require("../controllers/web.controller.js");
+//const webController  = require("../controllers/web.controller.js");
 
 
 const router = express.Router();
 
 // servicios libres
-router.post('/proceso/',webController.getEventosDet);
+//router.post('/proceso/',webController.getEventosDet);
 
 
 module.exports = router;
